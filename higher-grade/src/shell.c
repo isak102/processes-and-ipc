afab9e8cdfaad0412afbd0cb229c20c1287a53fa
TheGreenPlanet <48806927+TheGreenPlanet@users.noreply.github.com>
2023-02-13 16:39:03 +0100

    Add support for running two commands

--- higher-grade/src/shell.c
+++ higher-grade/src/shell.c
@@ -1,4 +1,4 @@
-#include "parser.h"    // cmd_t, position_t, parse_commands()
+#include "parser.h" // cmd_t, position_t, parse_commands()
 
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -8,9 +8,9 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <fcntl.h>     //fcntl(), F_GETFL
+#include <fcntl.h> //fcntl(), F_GETFL
 
-#define READ  0
+#define READ 0
 #define WRITE 1
 
 /**
@@ -22,8 +22,10 @@ cmd_t commands[MAX_COMMANDS];
 /**
  *  Debug printout of the commands array.
  */
-void print_commands(int n) {
-  for (int i = 0; i < n; i++) {
+void print_commands(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
     printf("==> commands[%d]\n", i);
     printf("  pos = %s\n", position_to_string(commands[i].pos));
     printf("  in  = %d\n", commands[i].in);
@@ -31,165 +33,194 @@ void print_commands(int n) {
 
     print_argv(commands[i].argv);
   }
-
 }
 
 /**
  * Returns true if file descriptor fd is open. Otherwise returns false.
  */
-int is_open(int fd) {
+int is_open(int fd)
+{
   return fcntl(fd, F_GETFL) != -1 || errno != EBADF;
 }
 
-void fork_error() {
+void fork_error()
+{
   perror("fork() failed)");
   exit(EXIT_FAILURE);
 }
 
-void pipe_error() {
+void pipe_error()
+{
   perror("pipe() failed)");
   exit(EXIT_FAILURE);
 }
 
-void dup2_error() {
+void dup2_error()
+{
   perror("dup2() failed)");
   exit(EXIT_FAILURE);
 }
 
+void redirect_file_descriptors(int *fd, unsigned int command_index)
+{
+  cmd_t *cmd = &commands[command_index];
+
+  switch (cmd->pos)
+  {
+  case first:
+    // Close the pipe read descriptor.
+    close(fd[READ]);
+
+    if (dup2(fd[WRITE], STDOUT_FILENO) == -1)
+    {
+      dup2_error();
+    }
+    cmd->out = fd[WRITE];
+
+    // Close the dangling pipe write descriptor.
+    close(fd[WRITE]);
+    break;
+
+  case last:
+
+    close(commands[command_index - 1].fd[WRITE]);
+
+    if (dup2(commands[command_index - 1].fd[READ], STDIN_FILENO) == -1)
+    {
+      dup2_error();
+    }
+    cmd->in = commands[command_index - 1].fd[READ];
+
+    // Close the dangling pipe read descriptor.
+    close(commands[command_index - 1].fd[READ]);
+    break;
+
+  case middle:
+    //puts("Middle child");
+    //printf("fd WRITE %d\n", fd[WRITE]);
+
+    // Close the pipe read descriptor.
+    close(fd[READ]);
+
+    if (dup2(commands[command_index - 1].out, cmd->in) == -1)
+    {
+      //puts("Middle child 1");
+
+      dup2_error();
+    }
+    if (dup2(fd[WRITE], cmd->out) == -1)
+    {
+      //puts("Middle child2 ");
+
+      dup2_error();
+    }
+    cmd->in = commands[command_index - 1].out;
+    cmd->out = fd[WRITE];
+
+    // Close both the dangling pipe read and write descriptors.
+    close(fd[WRITE]);
+    //printf("middle child closed!\n");
+
+    break;
+  default:
+    // unknown
+    // single
+    break;
+  }
+}
 
 /**
  *  Fork a proccess for command with index i in the command pipeline. If needed,
  *  create a new pipe and update the in and out members for the command..
  */
-void fork_cmd(int i) {
+void fork_cmd(int i)
+{
   pid_t pid;
   int fd[2];
-  
+
   // theres no need for a pipe if we have a single command
 
-  bool create_pipes = (commands[i].pos == first) || (commands[i].pos == middle); 
-  if (create_pipes) {
+  bool create_pipes = (commands[i].pos == first) || (commands[i].pos == middle);
+  if (create_pipes)
+  {
     puts("Pipe created");
-    if (pipe(fd) == -1) {
+    if (pipe(fd) == -1)
+    {
       pipe_error();
     }
+    commands[i].fd[READ] = fd[READ];
+    commands[i].fd[WRITE] = fd[WRITE];
   }
 
-  switch (pid = fork()) {
-    case -1:
-      fork_error();
-    case 0:
-      // Child process after a successful fork().
-      switch(commands[i].pos) {
-        case first:
-          puts("First child");
-          // Close the pipe read descriptor.
-          close(fd[READ]);
+  switch (pid = fork())
+  {
+  case -1:
+    fork_error();
+    break;
+  case 0:
+    // Child process after a successful fork().
+    redirect_file_descriptors(fd, i);
+    // Execute the command in the contex of the child process.
+    execvp(commands[i].argv[0], commands[i].argv);
 
-          if (dup2(fd[WRITE], commands[i].out) == -1) {
-            dup2_error();
-          }
-          commands[i].out = fd[WRITE];
-          
-          // Close the dangling pipe write descriptor.
-          close(fd[WRITE]);
-          printf("first child closed!\n");
-          break;
+    // If execvp() succeeds, this code should never be reached.
+    fprintf(stderr, "shell: command not found: %s\n", commands[i].argv[0]);
+    exit(EXIT_FAILURE);
+    break;
+  default:
+    // Parent process after a successful fork().
+    break;
+  }
+}
 
-        case last:
-          puts("Last child");
-          // Close the pipe read descriptor.
-          close(fd[WRITE]);
-
-          if (dup2(commands[i-1].out, commands[i].in) == -1) {
-            dup2_error();
-          }
-          commands[i].in = commands[i-1].out;
-
-          // Close the dangling pipe read descriptor.
-          close(fd[READ]);
-          // close(commands[i-1].out);
-          printf("last child closed!\n");
-          break;
-
-        case middle:
-          puts("Middle child");
-          printf("fd WRITE %d\n", fd[WRITE]);
-          
-          // Close the pipe read descriptor.
-          close(fd[READ]);
-
-          if (dup2(commands[i-1].out, commands[i].in) == -1) {
-            dup2_error();
-          }
-          if (dup2(fd[WRITE], commands[i].out) == -1) {
-            dup2_error();
-          }
-          commands[i].in = commands[i-1].out;
-          commands[i].out = fd[WRITE];
-
-          // Close both the dangling pipe read and write descriptors.
-          close(fd[WRITE]);
-          printf("middle child closed!\n");
-
-          break;
-        default:
-          // unknown
-          // single
-          break;
-      }
-
-      // Execute the command in the contex of the child process.
-      execvp(commands[i].argv[0], commands[i].argv);
-
-      // If execvp() succeeds, this code should never be reached.
-      fprintf(stderr, "shell: command not found: %s\n", commands[i].argv[0]);
-      exit(EXIT_FAILURE);
-
-    default:
-      // Parent process after a successful fork().
-      if (create_pipes) {
-        close(fd[READ]);
-        close(fd[WRITE]);
-      }
-      break;
+void close_all_pipes(int number_of_commands) {
+  for (int i=0; i < number_of_commands; i++) {
+    if ((commands[i].pos == first) || (commands[i].pos == middle)) {
+      close(commands[i].fd[READ]);
+      close(commands[i].fd[WRITE]);
+    }
   }
 }
 
 /**
  *  Fork one child process for each command in the command pipeline.
  */
-void fork_commands(int n) {
-
-  for (int i = 0; i < n; i++) {
+void fork_commands(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
     fork_cmd(i);
   }
+
+  close_all_pipes(n);
 }
 
 /**
  *  Reads a command line from the user and stores the string in the provided
  *  buffer.
  */
-void get_line(char* buffer, size_t size) {
+void get_line(char *buffer, size_t size)
+{
   getline(&buffer, &size, stdin);
-  buffer[strlen(buffer)-1] = '\0';
+  buffer[strlen(buffer) - 1] = '\0';
 }
 
-void wait_for_all_cmds(int n) {
-  for(int i=0; i < n; i++) {
+void wait_for_all_cmds(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
     wait(NULL);
-    printf("Wait %d finished\n", i);
   }
-  // Not implemented yet!
 }
 
-int main() {
-  int n;               // Number of commands in a command pipeline.
-  size_t size = 128;   // Max size of a command line string.
-  char line[size];     // Buffer for a command line string.
+int main()
+{
+  int n;             // Number of commands in a command pipeline.
+  size_t size = 128; // Max size of a command line string.
+  char line[size];   // Buffer for a command line string.
 
-
-  while(true) {
+  while (true)
+  {
     printf(" >>> ");
 
     get_line(line, size);
@@ -197,7 +228,6 @@ int main() {
     n = parse_commands(line, commands);
 
     fork_commands(n);
-    // print_commands(n);
 
     wait_for_all_cmds(n);
   }
